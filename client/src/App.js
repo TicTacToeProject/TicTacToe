a0b77e81246f5a690834196e144a38b40aef87c2
emanabuwaked <emanabuwaked@gmail.com>
2020-10-11 22:06:48 +0300

    TicTacToe

---
+++ client/src/App.js
@@ -0,0 +1,120 @@
+// import React from 'react';
+import React, { Component } from 'react';
+import ChoosePlayer from "./components/ChoosePlayer"
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       board: Array(9).fill(null),
+       player: null, 
+       winner: null
+    }
+  }
+  
+   checkWinner() {
+    let winLines =
+      [
+        ["0", "1", '2'],
+        ["3", "4", '5'],
+        ["6", "7", '8'],
+        ["0", "3", '6'],
+        ["1", "4", '7'],
+        ["2", "5", '8'],
+        ["0", "4", '8'],
+        ["2", "4", '6'],
+      ]
+     for (var index =0; index<winLines.length; index++){
+       const [a,b,c] =winLines[index]
+       //console.log(winLines[index])
+       var board = this.state.board
+       if (board[a] && board[a] === board[b] && board[a] === board[c]){
+         console.log(board[a]+"board[a]")
+         console.log(board[a]+"board[b]")
+         console.log(board[a]+"board[c]")
+         alert("you won")
+         this.setState({
+           winner: this.state.player
+         })
+       }
+     }
+
+  }
+
+
+handleClick (index){
+  if (this.state.player && !this.state.winner){
+    
+//console.log(index )
+const newBoard = this.state.board;
+if (newBoard[index] ===null ){
+newBoard[index]=this.state.player;
+// const newPlayer = this.state.player ==="x"? "o":"x"
+this.setState({
+  board: newBoard,
+  // player:newPlayer
+  player:this.state.player ==="x"? "o":"x"
+  
+})
+this.checkWinner();
+}
+// console.log(this.state.board)
+
+  }
+
+}
+
+    render() {
+        if (this.props.winner) {
+            return (<h2>Winner is {this.state.winner}</h2>)
+        } else {
+            return this.props.player ?
+                <h2>Next player is {this.state.player}</h2> :
+                <ChoosePlayer player={(e) => this.handleSetPlayer(e)} />
+        }
+    }
+
+setPlayer(player){
+//console.log(player)
+this.setState({
+  player:player
+})
+}
+
+handleButtonClick = () => {
+    this.form.reset() // resets "username" field to "admin"
+  }
+
+
+  render() {
+  const Box = this.state.board.map((box,index)=><div className="box" key={index}
+  onClick={()=>this.handleClick(index)} >
+     {box}</div>)
+  const status = this.state.player ? <p>Next player is {this.state.player}</p> :<ChoosePlayer player={(event)=>this.setPlayer(event)}/>
+    return (
+      <form ref={form => this.form = form}>
+      <div className="container">
+    <h1>Tic Tac Toe Game</h1> 
+    {status}
+    <div className="board">
+      {Box}
+    {/* <div className="box"> </div>
+    <div className="box"> </div>
+    <div className="box"> </div>
+    <div className="box"> </div>
+    <div className="box"> </div>
+    <div className="box"> </div>
+    <div className="box"> </div>
+    <div className="box"> </div>
+    <div className="box"> </div> */}
+    <button className="again" onClick={this.handleButtonClick}>Try Again</button>
+    </div>
+    </div>
+    </form>
+    )
+  }
+}
+
+export default App
---
+++ client/src/components/ChoosePlayer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export class choosePlayer extends Component {
+    handleForm(event){
+        event.preventDefault()
+        console.log(event.target.player.value)
+        console.log(this.props.player(event.target.player.value))
+        //console.log(event.target.player + "player"  )
+    }
+    render() {
+        return (
+         <form onSubmit= {(event)=>{this.handleForm(event)}}>
+             <p>Choose your weapon</p>
+             <label> X </label>
+             <input type="radio" name="player" id="X" value="X"/>
+             <br/>
+             <label> O </label>
+             <input type="radio" name="player" id="O" value="O"/>
+             <br/>
+
+             <input className="submit" type ="submit" value ="Start"/>
+         </form>
+        )
+    }
+}
+
+export default choosePlayer
